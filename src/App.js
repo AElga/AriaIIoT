57e42a4e8c2a2e9599beff9003cfad1a3724871d
mariaamashraaf <127663272+mariaamashraaf@users.noreply.github.com>
2024-07-01 16:42:33 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {apiResponse: ""};
+  }
+  callAPI() {
+    fetch("http://localhost:5000/test")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }))
+      .catch(err => console.error("Fetch error: ", err));
+  }
+  
+  componentDidMount() {
+    this.callAPI();
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>{this.state.apiResponse}</p>
+        </header>
+      </div>
+    );
+  }
+}
+export default App;
