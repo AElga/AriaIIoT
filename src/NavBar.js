d94232e47abc2d81958fb089f5016f4111b85eb4
Ali Elgazzar <121975641+AElga@users.noreply.github.com>
2024-07-21 06:24:43 -0700

    Homepage and Navbar changes

--- src/NavBar.js
+++ src/NavBar.js
@@ -31,13 +31,13 @@ const NavBar = () => {
             <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Fake Energy Alarm</a></li>
             </ul>
-            <NavLink className="nav-link ms-4" to="/MP">
+            {/* <NavLink className="nav-link ms-4" to="/MP" >
               <h4 class="font">Performance Monitoring</h4>
             </NavLink>
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"></a>
             <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Fake MP Alarm</a></li>
-            </ul>
+            </ul> */}
         </div>
         
       </div>
@@ -45,6 +45,16 @@ const NavBar = () => {
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"  />
       <button class="btn btn-outline-light btn-bg-dark" type="submit"  >Search</button>
     </form>
+    {/* <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body"></div>
+        </div> */}
     {/* <div>
     <nav class="navbar mh-75  fixed-bottom" style={{backgroundColor: "#999698"}}>
   <div class="container-fluid h-100" style={{backgroundColor: "#999698"}}>
